d660597a253369948b6d032cd2cc779f5ac5a999
tsar-boomba <itg.2048@gmail.com>
2024-05-01 10:45:30 -0400

    update deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
 import mdx from '@astrojs/mdx';
 import robotsTxt from 'astro-robots-txt';
 
@@ -10,7 +9,7 @@ import type { Root } from 'mdast';
 import { createCssVariablesTheme, getHighlighter } from 'shiki';
 import type { Highlighter, BuiltinLanguage } from 'shiki';
 import { visit } from 'unist-util-visit';
-
+import playformCompress from '@playform/compress';
 const SUPPORTED_LANGS = [
 	'javascript',
 	'typescript',
@@ -23,19 +22,16 @@ const SUPPORTED_LANGS = [
 	'mdx',
 	'astro',
 ] as const satisfies BuiltinLanguage[];
-
 type BlockParams = {
 	file?: string;
 	noBadge?: boolean;
 };
-
 const myTheme = createCssVariablesTheme({
 	name: 'css-variables',
 	variablePrefix: '--shiki-',
 	variableDefaults: {},
 	fontStyle: true,
 });
-
 let highlighterCache: Promise<Highlighter>;
 const parseLangRe = /(?=(?:file=(?<file>\S*))?)(?=(?:noBadge=(?<noBadge>true|false))?)/g;
 export const remarkCustomCodeBlock: () => Promise<Plugin<any[], Root>> = async () => {
@@ -45,39 +41,58 @@ export const remarkCustomCodeBlock: () => Promise<Plugin<any[], Root>> = async (
 			langs: SUPPORTED_LANGS,
 		});
 	const highlighter = await highlighterCache;
-
 	return () => (tree) => {
 		visit(tree, 'code', (node) => {
 			const rawLang = node.lang ?? 'plaintext';
 			const metaMatch = parseLangRe.exec(node.meta ?? '');
-
 			if (!metaMatch) {
 				return console.warn(
 					'Meta section of code fence should match format <file (optional)> <noBadge (true or false, optional)',
 				);
 			}
-
 			const { file, noBadge = false } = metaMatch.groups as unknown as BlockParams;
-
-			const langColorMap: { [K in BuiltinLanguage]?: { bg: string; fg: string } } = {
-				css: { bg: '#264de4', fg: '#fff' },
-				ts: { bg: '#007acc', fg: '#fff' },
-				tsx: { bg: '#007acc', fg: '#fff' },
-				java: { bg: '#ed8b00', fg: '#fff' },
-				rust: { bg: '#dea584', fg: '#fff' },
-				astro: { bg: '#ff5a03', fg: '#fff' },
-				toml: { bg: '#9c4221', fg: '#fff' },
+			const langColorMap: {
+				[K in BuiltinLanguage]?: {
+					bg: string;
+					fg: string;
+				};
+			} = {
+				css: {
+					bg: '#264de4',
+					fg: '#fff',
+				},
+				ts: {
+					bg: '#007acc',
+					fg: '#fff',
+				},
+				tsx: {
+					bg: '#007acc',
+					fg: '#fff',
+				},
+				java: {
+					bg: '#ed8b00',
+					fg: '#fff',
+				},
+				rust: {
+					bg: '#dea584',
+					fg: '#fff',
+				},
+				astro: {
+					bg: '#ff5a03',
+					fg: '#fff',
+				},
+				toml: {
+					bg: '#9c4221',
+					fg: '#fff',
+				},
 			};
-
 			const lang: BuiltinLanguage = (() => {
 				const lang = rawLang as BuiltinLanguage;
-
 				if (lang === 'typescript') return 'ts';
 				if (lang === 'javascript') return 'js';
 				if (lang === 'rs') return 'rust';
 				return lang;
 			})();
-
 			let codeHtml: string = highlighter.codeToHtml(node.value, {
 				lang,
 				theme: 'css-variables',
@@ -93,9 +108,7 @@ export const remarkCustomCodeBlock: () => Promise<Plugin<any[], Root>> = async (
 						!noBadge
 							? `<div
 							class='language-badge'
-							style='background-color:${langColorMap[lang]?.bg ?? '#888'};color:${
-								langColorMap[lang]?.fg ?? '#fff'
-							}'
+							style='background-color:${langColorMap[lang]?.bg ?? '#888'};color:${langColorMap[lang]?.fg ?? '#fff'}'
 						>
 							${lang.toUpperCase()}
 						</div>`
@@ -105,7 +118,6 @@ export const remarkCustomCodeBlock: () => Promise<Plugin<any[], Root>> = async (
 					</div>
 				</div>
 				`.trim();
-
 			node.value = html;
 			(node as any).type = 'html';
 			(node as any).children = [];
@@ -126,11 +138,11 @@ export default defineConfig({
 	// Enable Preact to support Preact JSX components.
 	integrations: [
 		sitemap(),
-		compress(),
 		mdx(),
 		robotsTxt({
 			sitemap: true,
 		}),
+		playformCompress(),
 	],
 	markdown: {
 		syntaxHighlight: false,
